39db69ab20d2fb3fdaa31274136ca0f7b68c3038
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2025-02-08 01:46:57 -0500

    learned condition

---
+++ conditions/index.js
@@ -0,0 +1,30 @@
+// let time = 12;
+
+// if ( time < 12) {
+//     console.log(`Good morning, it is ${time} AM`)
+// } else if ( time >= 12 && time < 18) {
+//     console.log(`Good afternoon, it is ${time} PM`)
+// } else if (time > 18 && time < 22) {
+//     console.log(`Good evening, it is ${time} PM.`)
+// } else {
+//     console.log("Good night")
+// }
+
+//Exercise
+//1 Create an empty variable named (password)
+//2 if the password is equal to 8, print "Welcome"
+//if the password is less then or equal to 8 print " Password is too short"
+//if password is greater or equal to 8 print "Password is too long" & "Password should be 8 characters"
+// If all fails print" provide a password.
+
+let myPassword;
+if ( myPassword === 8 ) {
+    console.log("Welcome")
+} else if ( myPassword <= 8 ) {
+    console.log("Password is too short")
+} else if (myPassword >= 8) {
+    console.log("Password is too long")
+} else {
+    console.log("Please provide a password")
+};
+
