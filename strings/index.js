d6e0c6f48a0aab5ba105b8b1eb044ac9a597121d
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2025-03-16 20:32:13 -0400

    learned split, join, and include

--- strings/index.js
+++ strings/index.js
@@ -22,6 +22,7 @@ console.log(`the name in uppercase is ${person.toUpperCase()}`)
 //Slice method in a string & Array 
 
 //In string: Takes 2 arguments: slice(x,y)  x: starts and y: end | x = index staring at 0 & y = position in the string example starting at 1 example 1,2,3 
+//Slice in string is used to extact substring efficiently
 
 let preferredFruit = "Mangoes"
 console.log(`the preffered fruit is ${preferredFruit.slice(0, 1)}`)
@@ -38,3 +39,37 @@ let domain = email.slice(4, 16);
 console.log(`the domain name is ${domain}`);
 console.log(`the second domain is ${secondDomain}`)
 
+//Split and join method 
+//Allows us to split and join :)
+
+// We are using template literals in this example. No Arrays returned
+let prefferedCourse = "Biology"
+let result = prefferedCourse.split("");
+let secondResult = prefferedCourse.split(" ")
+console.log(`The string splitted is ${result}`)
+console.log(`The second string is unchanged ? ${secondResult}`)
+
+
+//This will return an array because we are not using template literals
+let secondPreferedCourse = "Geometry";
+let thirdPreferedCourse = "Mathematic";
+let splittedString = secondPreferedCourse.split(""); 
+let splittedStringThird = thirdPreferedCourse.split(" ");
+console.log("The third prefered course is ", splittedString)  // split with no space split("") returns ["G", "e", "o", "m", "e", "t", "r", "y"]
+console.log("the second prefered course is ", splittedStringThird) // split with space spily(" ") returns ["Mathematic"]
+
+
+// //Working with the join() method 
+
+let numberOneCar = "Lambourgini";
+let numberTwoCar = "Ferrari";
+
+let joinedSecondCar = numberTwoCar.split(" ");
+let joinedString = numberOneCar.split("").join("-"); // This will insert - in the string
+console.log("Second car result ", joinedSecondCar)
+console.log(joinedString)
+
+//Working with includes()
+
+console.log(numberOneCar.includes("z")) //returns a b0olean false
+console.log(numberTwoCar.includes("F")) // returns a boolean true
